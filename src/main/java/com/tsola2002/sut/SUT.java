6f7298581eb2014ee9fde39841ffcdb37af37165
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-05-23 12:54:28 +0100

    added system u der test package

---
+++ src/main/java/com/tsola2002/sut/SUT.java
@@ -0,0 +1,25 @@
+package com.tsola2002.sut;
+
+public class SUT {
+  private String systemName;
+
+  // our constructor for this class
+  public SUT(String systemName) {
+    this.systemName = systemName;
+    System.out.println(systemName + " from class " + getClass().getSimpleName() + " is initializing.");
+  }
+
+  public boolean canReceiveRegularWork() {
+    System.out.println(systemName + " from class " + getClass().getSimpleName() + " can receive regular work.");
+    return true;
+  }
+
+  public boolean canReceiveAdditionalWork() {
+    System.out.println(systemName + " from class " + getClass().getSimpleName() + " cannot receive additional work.");
+    return false;
+  }
+
+  public void close() {
+    System.out.println(systemName + " from class " + getClass().getSimpleName() + " is closing.");
+  }
+}
---
+++ src/test/java/com/tsola2002/sut/SUTTest.java
@@ -0,0 +1,33 @@
+package com.tsola2002.sut;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SUTTest {
+
+  private static ResourceForAllTests resourceForAllTests;
+  private SUT systemUnderTest;
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void canReceiveRegularWork() {
+  }
+
+  @Test
+  public void canReceiveAdditionalWork() {
+  }
+
+  @Test
+  public void close() {
+  }
+}
