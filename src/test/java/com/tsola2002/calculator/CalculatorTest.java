109e6d0fb991e211fa4fa95e7296f854275e31f6
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-05-30 14:05:59 +0100

    added paremeterized tests

--- src/test/java/com/tsola2002/calculator/CalculatorTest.java
+++ src/test/java/com/tsola2002/calculator/CalculatorTest.java
@@ -2,14 +2,48 @@ package com.tsola2002.calculator;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class CalculatorTest {
+
+  private int input1;
+  private int input2;
+  private int expectedResult;
+
+  public CalculatorTest(int input1, int input2, int expectedResult) {
+    this.input1 = input1;
+    this.input2 = input2;
+    this.expectedResult = expectedResult;
+  }
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        { 1, 1, 2 },
+        { 2, 3, 5 },
+        { 3, 7, 10 },
+        { 4, 5, 9 },
+        { 5, 5, 10 }
+    });
+  }
+
+  @Test
+  public void testAddition() {
+    Calculator calc = new Calculator();
+    assertEquals(expectedResult, calc.add(input1, input2));
+  }
 
   @Test
   public void add() {
     Calculator calculator = new Calculator();
     Assert.assertEquals(7, calculator.add(3,4));
   }
+
+
 }
--- src/test/java/com/tsola2002/sut/SUTTest.java
+++ src/test/java/com/tsola2002/sut/SUTTest.java
@@ -2,16 +2,24 @@ package com.tsola2002.sut;
 
 import static org.junit.Assert.*;
 
+import com.tsola2002.calculator.Calculator;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
 
 public class SUTTest {
   private static ResourceForAllTests resourceForAllTests;
   private SUT systemUnderTest;
+  private int input1;
+  private int input2;
+  private int expectedResult;
+
 
   @BeforeClass
   public static void setUpClass() {
@@ -33,9 +41,15 @@ public class SUTTest {
     assertEquals("2 is not equal to 1", 2, 1);
   }
 
+
+  @Test(timeout = 4000)
+  public void testLongRunningMethod() throws InterruptedException {
+    SUT mySut = new SUT("long running system");
+    mySut.longRunningMethod();
+  }
+
   @AfterClass
   public static void tearDownClass() {
-
     resourceForAllTests.close();
   }
 
